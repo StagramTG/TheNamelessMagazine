bd7d4669cced00b444ace383797f70a4fc23d0a0
Thomas Gredin <t.gredin@gmx.fr>
2018-02-03 18:49:06 +0100

    add form structure in users edit view

--- fuel/app/modules/admin/views/admin/users/edit.blade.php
+++ fuel/app/modules/admin/views/admin/users/edit.blade.php
@@ -4,5 +4,34 @@
     <h1 class="title">Utilisateurs</h1>
     <p class="subtitle">Edition d'un utilisateur</p>
 
+    <form action="/admin/users/edit" method="POST">
 
+        <div class="field">
+            <p class="control">
+                <label for="username" class="label">Nom de l'utilisateur (Affiché sur le site)</label>
+                <input class="input" type="text" name="username" id="username" value="{{-- $article != null ? $article->title: "" --}}">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <label for="email" class="label">Adresse email</label>
+                <input class="input" type="email" name="email" id="email" value="{{-- $article != null ? $article->title: "" --}}">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <label for="password" class="label">Mot de passe</label>
+                <input class="input" type="password" name="password" id="password" value="{{-- $article != null ? $article->title: "" --}}">
+            </p>
+        </div>
+
+        <div class="field">
+            <p class="control">
+                <button class="button is-link is-outlined">Enregistrer</button>
+            </p>
+        </div>
+
+    </form>
 @endsection
