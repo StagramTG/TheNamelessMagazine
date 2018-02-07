59d334cec4199f1e195fdef39814f979d38834a0
Thomas Gredin <t.gredin@gmx.fr>
2018-02-07 22:02:59 +0100

    Remove quill.js from project

--- fuel/app/views/articles/read.blade.php
+++ fuel/app/views/articles/read.blade.php
@@ -4,7 +4,7 @@
 
     <div class="columns is-centered">
         <div class="column is-8-desktop">
-            <figure class="image is-clipped" style="max-height: 50vh">
+            <figure class="image is-clipped" style="max-height: 50vho">
                 <img src="{{ $article->image }}">
             </figure>
             <h1 class="title">{{ $article->title }}</h1>
