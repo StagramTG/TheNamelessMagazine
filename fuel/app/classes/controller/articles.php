c3f40c65ac6965d42d17b3d41a0bc90f0296f57a
Thomas Gredin <t.gredin@gmx.fr>
2018-02-08 11:50:58 +0100

    Add articles by category page with pagination

--- fuel/app/classes/controller/articles.php
+++ fuel/app/classes/controller/articles.php
@@ -7,6 +7,8 @@
 
 namespace Controller;
 
+use Orm\Model;
+
 class Articles extends \Fuelblade\Controller\Blade
 {
     public function action_index()
@@ -38,6 +40,52 @@ class Articles extends \Fuelblade\Controller\Blade
                 ->related('categories')
                 ->order_by('created_at', 'desc')
                 ->where('draft', '=', 0)
+                ->limit($article_by_page)
+                ->get();
+        }
+
+        $data['page'] = 'articles';
+        return $this->view('articles/index', $data);
+    }
+
+    public function action_category()
+    {
+        $article_by_page = 10;
+
+        $page = intval(\Input::get('page'));
+        $category_id = \Input::get('id');
+
+        $data = array();
+        $data['page_index'] = 0;
+        $article_count = \Model\Article::query()
+            ->related('categories')
+            ->where('draft', '=', 0)
+            ->where('category_id', '=', $category_id)
+            ->count();
+        $data['page_count'] = ceil($article_count / $article_by_page) - 1;
+
+        if($page != null)
+        {
+            $data['articles'] = \Model\Article::query()
+                ->related('users')
+                ->related('categories')
+                ->order_by('created_at', 'desc')
+                ->where('draft', '=', 0)
+                ->where('category_id', '=', intval($category_id))
+                ->offset($page * $article_by_page)
+                ->limit($article_by_page)
+                ->get();
+
+            $data['page_index'] = $page;
+        }
+        else
+        {
+            $data['articles'] = \Model\Article::query()
+                ->related('users')
+                ->related('categories')
+                ->order_by('created_at', 'desc')
+                ->where('draft', '=', 0)
+                ->where('category_id', '=', intval($category_id))
                 ->limit($article_by_page)
                 ->get();
         }
